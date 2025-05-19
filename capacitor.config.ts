273161ca677aed6643bad683b7d7c87799d00959
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-05-19 14:07:24 +0200

    chore: remove all application files and configurations for a fresh start

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'mounts-menagerie-app',
-  webDir: 'www'
-};
-
-export default config;
