0e2271360da69a406c60f6d9bbe35e5d1d62a1fe
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-05-19 14:54:07 +0200

    feat: enhance app component with mounts display and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonToolbar, IonHeader, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  standalone: true,
+  imports: [IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCol, IonRow, IonGrid, IonContent, IonTitle, IonHeader, IonToolbar, IonApp, ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {}
+  mounts = [
+    {
+      name: 'Invincible',
+      image: 'https://wow.zamimg.com/uploads/screenshots/normal/907610.jpg',
+      description: 'The steed of Arthas Menethil, raised in undeath.'
+    },
+    {
+      name: 'Ashes of Al\'ar',
+      image: 'https://wow.zamimg.com/uploads/screenshots/normal/867678.jpg',
+      description: 'A phoenix of unmatched beauty and power.'
+    },
+    {
+      name: 'Rivendare\'s Deathcharger',
+      image: 'https://wow.zamimg.com/uploads/screenshots/normal/954163.jpg',
+      description: 'A rare undead warhorse from Stratholme.'
+    }
+  ];
 }
