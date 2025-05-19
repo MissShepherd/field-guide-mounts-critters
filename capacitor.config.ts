5965dc10451b4c180d51a9bf51c84402986ea8c7
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-05-19 14:46:41 +0200

    feat: initialize Ionic Angular application with basic structure and routing
    - Created package.json with dependencies for Angular and Ionic.
    - Added app component with basic HTML structure.
    - Implemented routing with a home page.
    - Developed home page with a simple layout and styles.
    - Added environment configuration files for production and development.
    - Included global styles and polyfills for the application.
    - Set up testing configurations for app and home components.
    - Added favicon and SVG assets for the application.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mounts-menagerie-app-ui',
+  webDir: 'www'
+};
+
+export default config;
