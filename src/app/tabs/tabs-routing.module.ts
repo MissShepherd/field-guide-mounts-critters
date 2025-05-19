be0617c62f05a5ff944295ddc1baa7dd139ab75e
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-05-19 13:22:57 +0200

    Implement tabs navigation with lazy loading for home, mounts, pets, and collection pages

---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('../pages/home/home.module').then(m => m.HomePageModule),
+      },
+      {
+        path: 'mounts',
+        loadChildren: () =>
+          import('../pages/mounts/mounts.module').then(m => m.MountsPageModule),
+      },
+      {
+        path: 'pets',
+        loadChildren: () =>
+          import('../pages/pets/pets.module').then(m => m.PetsPageModule),
+      },
+      {
+        path: 'collection',
+        loadChildren: () =>
+          import('../pages/collection/collection.module').then(m => m.CollectionPageModule),
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
