d2804a55b9dbec5789f5df6099d8e27caa2ef1d1
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-05-20 11:49:51 +0200

    chore: prepare for upcoming feature development by cleaning up code structure and removing unused files

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,29 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonToolbar, IonHeader, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from "@ionic/angular/standalone";
+import { IonicModule } from '@ionic/angular';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCol, IonRow, IonGrid, IonContent, IonTitle, IonHeader, IonToolbar, IonApp, ],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  template: `<ion-app><router-outlet /></ion-app>`,
+  imports: [IonicModule, RouterOutlet]
 })
-export class AppComponent {
-  mounts = [
-    {
-      name: 'Invincible',
-      image: 'https://wow.zamimg.com/uploads/screenshots/normal/907610.jpg',
-      description: 'The steed of Arthas Menethil, raised in undeath.'
-    },
-    {
-      name: 'Ashes of Al\'ar',
-      image: 'https://wow.zamimg.com/uploads/screenshots/normal/867678.jpg',
-      description: 'A phoenix of unmatched beauty and power.'
-    },
-    {
-      name: 'Rivendare\'s Deathcharger',
-      image: 'https://wow.zamimg.com/uploads/screenshots/normal/954163.jpg',
-      description: 'A rare undead warhorse from Stratholme.'
-    }
-  ];
-}
+export class AppComponent {}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 import { environment } from './environments/environment';
 
@@ -16,7 +15,7 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideHttpClient()
   ]
-}).catch(err => console.error(err));
+});
