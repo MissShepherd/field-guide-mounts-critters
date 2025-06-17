9c5bec225916902ed955547a05ee7f118858b9d6
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-06-17 15:56:23 +0200

    feat(more): add MorePage component with initial setup

---
+++ frontend/src/app/pages/more/more.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-more',
+  standalone: true,
+  imports: [IonicModule],
+  templateUrl: './more.page.html',
+  styleUrls: ['./more.page.scss'],
+})
+export class MorePage {}
