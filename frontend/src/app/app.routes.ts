9000614df4343e9e7148960beccdd343a585668b
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-06-17 14:01:10 +0200

    Refactor routing configuration to lazy-load HomePage and remove TabsPage structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,41 +1,11 @@
 import { Routes } from '@angular/router';
-import { TabsPage } from './tabs/tabs.page';
-import { HomePage } from './pages/home/home.page';
+
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomePage // Start page
-  },
-  {
-    path: 'tabs',
-    component: TabsPage,
-    children: [
-      {
-        path: 'mounts',
-        loadComponent: () =>
-          import('./pages/mounts/mounts.page').then(m => m.MountsPage),
-      },
-      {
-        path: 'pets',
-        loadComponent: () =>
-          import('./pages/pets/pets.page').then(m => m.PetsPage),
-      },
-      {
-        path: 'toys',
-        loadComponent: () =>
-          import('./pages/toys/toys.page').then(m => m.ToysPage),
-      },
-      {
-        path: 'more',
-        loadComponent: () =>
-          import('./pages/more/more.page').then(m => m.MorePage),
-      },
-      {
-        path: '',
-        redirectTo: 'mounts',
-        pathMatch: 'full',
-      },
-    ],
-  },
+    loadComponent: () =>
+      import('./pages/home/home.page').then(m => m.HomePage), // Lazy-load HomePage
+  }
+  
 ];
