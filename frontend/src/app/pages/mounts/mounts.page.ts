73ae5a75e2828f1063ca19521dcaeae40be09e52
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-06-17 15:51:50 +0200

    feat(mounts): create mounts page with background and placeholder text
    - Added HTML structure for the mounts page with a placeholder.
    - Implemented SCSS styles for the page background and placeholder text.
    - Created TypeScript component for the mounts page.

---
+++ frontend/src/app/pages/mounts/mounts.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-mounts',
+  standalone: true,
+  imports: [IonicModule],
+  templateUrl: './mounts.page.html',
+  styleUrls: ['./mounts.page.scss'],
+})
+export class MountsPage {}
