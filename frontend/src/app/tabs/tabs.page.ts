f521ac6b3263513b367f8769b9865150bc2ec8b0
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-07-02 12:53:47 +0200

    Refactor tab navigation and styles
    - Updated tabs.page.html to use ion-icon for tab buttons instead of images.
    - Simplified tab button structure and removed unnecessary routerLink attributes.
    - Enhanced tabs.page.scss with improved styling for the tab bar and buttons, including a gradient background and responsive design.
    - Adjusted tab-icon size for better alignment and aesthetics.

--- frontend/src/app/tabs/tabs.page.ts
+++ frontend/src/app/tabs/tabs.page.ts
@@ -9,13 +9,31 @@ import { CommonModule } from '@angular/common';
   imports: [IonicModule, RouterModule, CommonModule],
   template: `
     <ion-tabs>
-      <ion-tab-bar slot="bottom">
+      <ion-tab-bar slot="bottom" class="wow-tab-bar">
+
         <ion-tab-button tab="mounts" [routerLink]="['/tabs/mounts']">
-          <ion-icon name="paw-outline"></ion-icon>
+          <img src="assets/icons/mount_icon.png" class="tab-icon" />
           <ion-label>Mounts</ion-label>
         </ion-tab-button>
+
+        <ion-tab-button tab="pets" [routerLink]="['/tabs/pets']">
+          <img src="assets/icons/pet_icon.png" class="tab-icon" />
+          <ion-label>Pets</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="toys" [routerLink]="['/tabs/toys']">
+          <img src="assets/icons/toy_icon.png" class="tab-icon" />
+          <ion-label>Toys</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="more" [routerLink]="['/tabs/more']">
+          <img src="assets/icons/more_icon.png" class="tab-icon" />
+          <ion-label>More</ion-label>
+        </ion-tab-button>
+
       </ion-tab-bar>
     </ion-tabs>
-  `
+  `,
+  styleUrls: ['./tabs.page.scss']
 })
 export class TabsPage {}
