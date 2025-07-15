471888ac967a146b5a51d7d20dc7f17828d4a2f0
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-07-15 16:01:42 +0200

    lot of stuff

---
+++ frontend/.angular/cache/19.2.12/app/vite/deps/md.transition-7SZVADGK.js
@@ -0,0 +1,12 @@
+import {
+  mdTransitionAnimation
+} from "./chunk-DOTYFT23.js";
+import "./chunk-TETET2SC.js";
+import "./chunk-MGPHXSG2.js";
+import "./chunk-CJ5MJUPJ.js";
+import "./chunk-L6BHBXTE.js";
+import "./chunk-ZWBDDU4U.js";
+import "./chunk-ZVATTXSA.js";
+export {
+  mdTransitionAnimation
+};
