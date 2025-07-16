98e730d06ed376211f65b8ae80c98d68b1e5796c
tatjana.schaefer <tatjana.schaefer@santismail.ch>
2025-07-16 08:08:52 +0200

    feat: enhance pets page layout with character header and progress display

--- frontend/src/app/pages/pets/pets.page.ts
+++ frontend/src/app/pages/pets/pets.page.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { CharacterHeaderComponent } from '../../components/character-header/character-header.component';
 
 @Component({
   selector: 'app-pets',
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, CharacterHeaderComponent],
   templateUrl: './pets.page.html',
-  styleUrls: ['./pets.page.scss'],
+  styleUrls: ['./pets.page.scss']
 })
-export class PetsPage {}
+export class PetsPage {
+  pets = [
+    'Zergling',
+    'Mini Diablo',
+    'Panda Cub',
+    'Clockwork Gnome',
+    'Grunty',
+    'Murkalot',
+    'Magical Crawdad'
+  ];
+
+  petsCollected = 300;
+  totalPets = 1200;
+}
